aae3c008083417dfaf35348da39ee5627ff9f8c0
Laurens Sion <Laurens.Sion@cs.kuleuven.be>
2026-07-16 00:44:12 +0200

    fix: small rcp fixes and optimizations

--- bundles/be.kuleuven.cs.distrinet.sparta.rcp/src/be/kuleuven/cs/distrinet/sparta/rcp/SPARTAPerspective.java
+++ bundles/be.kuleuven.cs.distrinet.sparta.rcp/src/be/kuleuven/cs/distrinet/sparta/rcp/SPARTAPerspective.java
@@ -9,14 +9,32 @@
  */
 package be.kuleuven.cs.distrinet.sparta.rcp;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class SPARTAPerspective implements IPerspectiveFactory {
 
+	private static final String PROJECT_EXPLORER = "org.eclipse.ui.navigator.ProjectExplorer";
+	private static final String THREAT_ANALYSIS = "be.kuleuven.cs.distrinet.sparta.analysis.views.ThreatAnalysis";
+	private static final String PROPERTY_SHEET = "org.eclipse.ui.views.PropertySheet";
+	private static final String PROBLEM_VIEW = "org.eclipse.ui.views.ProblemView";
+
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		
+		String editorArea = layout.getEditorArea();
+		layout.setEditorAreaVisible(true);
+
+		// Project Explorer on the left of the editor area.
+		layout.addView(PROJECT_EXPLORER, IPageLayout.LEFT, 0.15f, editorArea);
+
+		// Threat Analysis on the right of the editor area.
+		layout.addView(THREAT_ANALYSIS, IPageLayout.RIGHT, 0.66f, editorArea);
+
+		// Property Sheet and Problems stacked in a folder below the editor area.
+		IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.75f, editorArea);
+		bottom.addView(PROPERTY_SHEET);
+		bottom.addView(PROBLEM_VIEW);
 	}
 
 }
